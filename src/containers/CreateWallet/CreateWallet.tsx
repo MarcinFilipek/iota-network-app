c27fffe83c30c5cbd152b6026c1cfdb116910f02
MarcinFilipek <filipekmarcin180@gmail.com>
2022-12-06 21:26:49 +0100

    Added dialog container

--- src/containers/CreateWallet/CreateWallet.tsx
+++ src/containers/CreateWallet/CreateWallet.tsx
@@ -1,18 +1,21 @@
 import { Outlet, Route, Routes } from "react-router-dom";
+import { Dialog } from "../Dialog";
 
 export const CreateWallet = () => (
-  <Routes>
-    <Route
-      path="/"
-      element={
-        <>
-          <p>Create wallet</p>
-          <Outlet />
-        </>
-      }
-    >
-      <Route index element={<p>Step 1</p>} />
-      <Route path="/2" element={<p>Step 2</p>} />
-    </Route>
-  </Routes>
+  <Dialog>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <p>Create wallet</p>
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<p>Step 1</p>} />
+        <Route path="/2" element={<p>Step 2</p>} />
+      </Route>
+    </Routes>
+  </Dialog>
 );
