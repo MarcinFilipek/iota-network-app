5867cef95783ba192c9ba1b97347d66d35f776e1
MarcinFilipek <filipekmarcin180@gmail.com>
2022-12-22 20:39:01 +0100

    Added loader (#4)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -14,6 +16,32 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (webpackConfig) => {
+    const cssRules = webpackConfig.module.rules.find((rule) => {
+      if (rule.test === undefined) {
+        return false;
+      }
+      return rule.test.toString() === "/\\.css$/";
+    });
+    // Remove the existing css rule
+    webpackConfig.module.rules = webpackConfig.module.rules.filter((rule) => {
+      if (rule.test === undefined) {
+        return true;
+      }
+      return rule.test.toString() !== "/\\.css$/";
+    });
+
+    const cssLoader = cssRules.use.find(
+      (e) => e.loader.search("css-loader") > -1
+    );
+    if (cssLoader) {
+      cssLoader.options = {
+        modules: true,
+      };
+    }
+    webpackConfig.module.rules.push({ ...cssRules, test: "/\\.css$/" });
+    return webpackConfig;
+  },
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
